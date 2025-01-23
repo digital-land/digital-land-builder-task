f9a9a5d4e00f1369286d735e64c7cadb686e8e5b
Christopher Johns <cjohns@scottlogic.com>
2025-01-23 10:13:30 +0000

    WIP.

--- task/run.sh
+++ task/run.sh
@@ -6,7 +6,7 @@ TODAY=$(date +%Y-%m-%d)
 echo "Running digital land builder on $TODAY"
 
 if [ -z "$READ_S3_BUCKET" ]; then
-    echo READ_S3_BUCKET not set so files will be downloaded from the production files cdn
+    echo READ_S3_BUCKET not set so files will be downloaded from the production files CDN
 fi
 
 if [ -z "$WRITE_S3_BUCKET" ]; then
@@ -17,7 +17,24 @@ fi
 make makerules
 make init
 
-# Run
+# Make Digital Land DB
+make clobber
 make
 
-# TODO: push the resulting files back to S3
+# Make Performance DB
+if [ "$BUILD_PERFOMANCE_DB" = "True" ]; then
+    make clobber-performance
+    make third-pass
+fi
+
+
+# Save the output to S3
+if [ -n "$WRITE_S3_BUCKET" ]; then
+    HOISTED_COLLECTION_DATASET_BUCKET_NAME=$WRITE_S3_BUCKET
+    COLLECTION_DATASET_BUCKET_NAME=$WRITE_S3_BUCKET
+    export HOISTED_COLLECTION_DATASET_BUCKET_NAME
+    export COLLECTION_DATASET_BUCKET_NAME
+    make save-dataset
+else
+    echo WRITE_S3_BUCKET not set so files not uploaded to S3
+fi
